b53201bd46a76c9edccce6234b9d3ba96cdc30c2
apzhang <apzhang@gmail.com>
2025-01-28 16:25:38 -0500

    updated instructions on readme

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/video-clipper",
-  output: "export",  // <=== enables static exports
+  // basePath: "/video-clipper",
+  // output: "export",  // <=== enables static exports
   reactStrictMode: false,
 };
 
-export default nextConfig
+export default nextConfig;
